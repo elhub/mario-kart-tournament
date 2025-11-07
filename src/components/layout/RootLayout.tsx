a91e5371dfaa5bf3b4d8f395b79c780341b565bd
Pål Oskar Engen <pal.engen@statnett.no>
2025-11-07 13:37:35 +0100

    chore: clean away stuff from template

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,14 +1,10 @@
 import { Outlet } from "react-router-dom";
 import { Flex } from "@chakra-ui/react";
-import Header from "@/components/header";
 
 const RootLayout = () => {
   return (
-    <Flex flexDir="column" minH="100vh">
-      <Header />
-      <Flex flexDir="column" align="center" gap="8" minH="calc(100vh - 48px)" w="full">
-        <Outlet />
-      </Flex>
+    <Flex flexDir="column" align="center" gap="8" minH="100vh" w="full">
+      <Outlet />
     </Flex>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "@/App";
 import RootLayout from "@/components/layout/RootLayout";
-import Mv from "@/pages/mv";
 import Error from "@/pages/error";
-import Tournament from "@/pages/tournament";
 
 const root = document.getElementById("root");
 
@@ -19,8 +17,6 @@ ReactDOM.createRoot(root!).render(
           <Routes>
             <Route element={<RootLayout />}>
               <Route index element={<App />} errorElement={<Error />} />
-              <Route path="mv" element={<Mv />} errorElement={<Error />} />
-              <Route path="tournament" element={<Tournament />} errorElement={<Error />} />
             </Route>
           </Routes>
         </BrowserRouter>
